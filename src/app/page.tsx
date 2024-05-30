2dfba38c825dcd73816ea529949de4fd0c7affbd
samuiel3986 <abebesamuiel@gmail.com>
2024-05-30 20:03:40 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import {NavbarDemo,} from "./test";
+import { PlaceholdersAndVanishInputDemo} from "./test2";
+import { SignupFormDemo} from "./test3";
+import { HeroParallaxDemo} from "./test4";
+import { FlipWordsDemo} from "./test5";
+import { GoogleGeminiEffectDemo} from "./test6";
+import { SparklesPreview} from "./test7";
+export default function Home() {
+  return (
+    <main>
+    <NavbarDemo></NavbarDemo>
+    <HeroParallaxDemo></HeroParallaxDemo>
+    <SparklesPreview></SparklesPreview>
+    {/* <PlaceholdersAndVanishInputDemo></PlaceholdersAndVanishInputDemo> */}
+    <SignupFormDemo></SignupFormDemo>
+    <FlipWordsDemo></FlipWordsDemo>
+    <GoogleGeminiEffectDemo></GoogleGeminiEffectDemo>
+    </main>
+  );
+}
